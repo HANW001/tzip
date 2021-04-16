dc3f43bc3a0c350122a499c093d9172a3e2aadb1
mthdus <80079613+mthdus@users.noreply.github.com>
2021-04-16 20:53:18 +0900

    Update chat_screen.dart

--- flutter/flutter_all/lib/chat_screen.dart
+++ flutter/flutter_all/lib/chat_screen.dart
@@ -1,34 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:gcm_app/message_model.dart';
-import 'package:gcm_app/user_model.dart';
+import 'package:flutterui_body_application/message_model.dart';
+import 'package:flutterui_body_application/user_model.dart';
 
+// massages
 class ChatScreen extends StatefulWidget {
   final User user;
+
   ChatScreen({this.user});
+
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
-  bool _visibility = true;
-  void _show() {
-    setState(() {
-      _visibility = true;
-    });
-  }
-  void _hide() {
-    setState(() {
-      _visibility = false;
-    });
-  }
-
-  _buildMessage(Message message, bool isMe) {
+  _buildMessage(Message message, int index, bool unread) {
     DateTime now = DateTime.now();
     String formattedDate = now.hour.toString() + ":" + now.minute.toString();
-    // dynamic currentTime = DateFormat.jm().format(DateTime.now());
     final Container msg = Container(
-      margin: isMe
+      margin: unread
           ? EdgeInsets.only(
               top: 8.0,
               bottom: 8.0,
@@ -39,10 +28,10 @@ class _ChatScreenState extends State<ChatScreen> {
               bottom: 8.0,
             ),
       padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
-      width: MediaQuery.of(context).size.width * 0.95,
+      width: MediaQuery.of(context).size.width * 0.75,
       decoration: BoxDecoration(
-        color: isMe ? Color(0xFFFFEFEE) : Color(0xFFF9FFF7),
-        borderRadius: isMe
+        color: unread ? Color(0xFFFFEFEE) : Color(0xFFF9FFF7),
+        borderRadius: unread
             ? BorderRadius.only(
                 topRight: Radius.circular(15.0),
                 bottomRight: Radius.circular(15.0),
@@ -58,7 +47,7 @@ class _ChatScreenState extends State<ChatScreen> {
           Text(
             message.time,
             style: TextStyle(
-              color: isMe ? Colors.red : Colors.blueGrey,
+              color: unread ? Colors.red : Colors.blueGrey,
               fontSize: 16.0,
               fontWeight: FontWeight.bold,
             ),
@@ -67,106 +56,51 @@ class _ChatScreenState extends State<ChatScreen> {
           Text(
             message.text,
             style: TextStyle(
-              color: isMe ? Colors.red : Colors.blueGrey,
+              color: unread ? Colors.red : Colors.blueGrey,
               fontSize: 16.0,
               fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(height: 10.0),
           Container(
             // ignore: deprecated_member_use
-            child: FlatButton(
-              child: Text(
-                "확인",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white,
-                ),
-              ),
-              color: Colors.lightGreen,
-              splashColor: Colors.cyan,
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              textColor: Colors.white,
-              onPressed: () {
-                var value = new Message(
-                  sender: khw,
-                  time: formattedDate, //time
-                  text: '[욕창] 자세 변경 완료',
-                  // isLiked: false,
-                  unread: true,
-                );
-                messages.insert(0, value);
-                setState(() {});
-              },
-            ),
+            child: unread
+                ? FlatButton(
+                    child: Text("확인"),
+                    color: Colors.lightGreen,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      messages[index].setUnread(false);
+                      messages[index].setText("변경완료");
+                      setState(() {});
+                    },
+                  )
+                : Text(" "),
           ),
         ],
       ),
     );
 
-    if (isMe) {
+    if (unread) {
       return msg;
     }
     return Row(
       children: <Widget>[
         msg,
-    //     IconButton(
-    //       icon: message.isLiked
-    //           ? Icon(Icons.favorite)
-    //           : Icon(Icons.favorite_border),
-    //       iconSize: 30.0,
-    //       color: message.isLiked
-    //           ? Theme.of(context).primaryColor
-    //           : Colors.blueGrey,
-    //       onPressed: () {},
-    //     )
       ],
     );
   }
 
-  // _buildMessageComposer() {
-  //   return Container(
-  //     padding: EdgeInsets.symmetric(horizontal: 8.0),
-  //     height: 70.0,
-  //     color: Colors.white,
-  //     child: Row(
-  //       children: <Widget>[
-  //         IconButton(
-  //           icon: Icon(Icons.photo),
-  //           iconSize: 25.0,
-  //           color: Theme.of(context).primaryColor,
-  //           onPressed: () {},
-  //         ),
-  //         Expanded(
-  //           child: TextField(
-  //             textCapitalization: TextCapitalization.sentences,
-  //             onChanged: (value) {},
-  //             decoration: InputDecoration.collapsed(
-  //               hintText: 'Send a message...',
-  //             ),
-  //           ),
-  //         ),
-  //         IconButton(
-  //           icon: Icon(Icons.send),
-  //           iconSize: 25.0,
-  //           color: Theme.of(context).primaryColor,
-  //           onPressed: () {},
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = now.hour.toString() + ":" + now.minute.toString();
+
     return Scaffold(
       backgroundColor: Color(0xFF95CD72),
       appBar: AppBar(
         toolbarHeight: 65,
         centerTitle: true,
-        iconTheme: IconThemeData(
-            color: Colors.white
-        ),
+        iconTheme: IconThemeData(color: Colors.white),
         title: Text(
           widget.user.name,
           style: TextStyle(
@@ -222,8 +156,8 @@ class _ChatScreenState extends State<ChatScreen> {
                     itemCount: messages.length,
                     itemBuilder: (BuildContext context, int index) {
                       final Message message = messages[index];
-                      final bool isMe = message.sender.id == currentUser.id;
-                      return _buildMessage(message, isMe);
+                      return _buildMessage(message, index, message.unread);
+                  
                     },
                   ),
                 ),
@@ -233,6 +167,3 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
-    );
-  }
-}
