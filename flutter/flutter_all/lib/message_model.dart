dfb662e09553d0c5ff5ba1b6d3190bad3babeeee
mthdus <80079613+mthdus@users.noreply.github.com>
2021-04-16 20:54:34 +0900

    Update message_model.dart

--- flutter/flutter_all/lib/message_model.dart
+++ flutter/flutter_all/lib/message_model.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:gcm_app/user_model.dart';
+import 'package:flutterui_body_application/user_model.dart';
 
 class Message {
   final User sender;
   final String
       time; // Would usually be type DateTime or Firebase Timestamp in production apps
-  final String text;
+  String text;
+
   // final bool isLiked;
-  final bool unread;
+  bool unread;
 
   Message({
     this.sender,
@@ -16,6 +16,14 @@ class Message {
     // this.isLiked,
     this.unread,
   });
+
+  setUnread(bool unread){
+    this.unread = unread;
+  }
+
+  setText(String text){
+    this.text = text;
+  }
 }
 
 // YOU - current user
@@ -96,56 +104,15 @@ List<Message> chats = [
   ),
 ];
 
-// EXAMPLE MESSAGES IN CHAT SCREEN
+DateTime now = DateTime.now();
+String formattedDate = now.hour.toString() + ":" + now.minute.toString();
 List<Message> messages = [
+
   Message(
     sender: currentUser,
-    time: '3:34 PM',
+    time: formattedDate,
     text: '[욕창] 자세를 변경해주세요',
     // isLiked: true,
     unread: true,
-  ),
-//   Message(
-//     sender: khw,
-//     time: '3:04 PM',
-//     text: '[욕창] 자세 변경 완료',
-//     // isLiked: true,
-//     unread: true,
-//   ),
-//   Message(
-//     sender: khw,
-//     time: '3:03 PM',
-//     text: '[욕창] 자세를 변경해주세요',
-//     // isLiked: true,
-//     unread: true,
-//   ),
-//   Message(
-//     sender: kjh,
-//     time: '2:33 PM',
-//     text: '[욕창] 자세 변경 완료',
-//     // isLiked: true,
-//     unread: true,
-//   ),
-//   Message(
-//     sender: kjh,
-//     time: '2:32 PM',
-//     text: '[욕창] 자세를 변경해주세요',
-//     // isLiked: true,
-//     unread: true,
-//   ),
-//   Message(
-//     sender: aes,
-//     time: '2:02 PM',
-//     text: '[욕창] 자세 변경 완료',
-//     // isLiked: true,
-//     unread: true,
-//   ),
-//   Message(
-//     sender: aes,
-//     time: '2:00 PM',
-//     text: '[욕창] 자세를 변경해주세요',
-//     // isLiked: true,
-//     unread: true,
-//   ),
-];
+  )];
 
